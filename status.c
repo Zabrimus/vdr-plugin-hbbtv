25f63f495acc9ce813c859bfbf3223617de2fd1a
Zabrimus <github@hannebauer.org>
2020-06-02 16:52:08 +0200

    send current channel to browser

--- status.c
+++ status.c
@@ -10,7 +10,7 @@
 #include "ait.h"
 #include "hbbtvurl.h"
 #include <vdr/channels.h>
-
+#include "browsercommunication.h"
 
 cHbbtvDeviceStatus::cHbbtvDeviceStatus()
 {
@@ -46,12 +46,14 @@ void cHbbtvDeviceStatus::ChannelSwitch(const cDevice * vdrDevice, int channelNum
          device = cDevice::ActualDevice();
 #if APIVERSNUM >= 20301
          LOCK_CHANNELS_READ
-         sid = Channels->GetByNumber(channelNumber)->Sid();
-         const char* currentChannel = Channels->GetByNumber(channelNumber)->Name();
+         auto channel = Channels->GetByNumber(channelNumber);
+         sid = channel->Sid();
+         const char* currentChannel = channel->Name();
 #else
-         const char* currentChannel = Channels->GetByNumber(channelNumber)->Name();
+         auto channel = Channels->GetByNumber(channelNumber);
+         sid = channel->Sid();
+         const char* currentChannel = channel->Name();
 #endif
-
          // find all well known URLs
          char* search;
          asprintf(&search, "%s~~~", currentChannel);
@@ -68,6 +70,32 @@ void cHbbtvDeviceStatus::ChannelSwitch(const cDevice * vdrDevice, int channelNum
 
          device->AttachFilter(aitFilter = new cAitFilter(sid));
          isyslog("[hbbtv] Attached HbbTV ait filter to device %d, vdrDev=%d actDev=%d, Sid=0x%04x", device->CardIndex()+1, vdrDevice->CardIndex()+1,cDevice::ActualDevice()->CardIndex()+1, sid);
+
+         // inform browser about the channel switch
+
+         // longName, Name => currentChannel
+         // nid            => ??? (use 1 as default)
+         // onid           => channel, Nid
+         // sid            => channel, Sid
+         // tsid           => channel, Tid
+         // channelType    => HDTV 0x19, TV 0x01, Radio 0x02
+         // idType         => ??? (use 15 as default)
+
+         int channelType;
+
+         if (strstr(currentChannel, "HD") != NULL) {
+             channelType = 0x19;
+         } else if (channel->Rid() > 0) {
+             channelType = 0x02;
+         } else {
+             channelType = 0x01;
+         }
+
+         char *cmd;
+         asprintf(&cmd, "CHANNEL {\"channelType\":%d,\"ccid\":\"ccid://1.0\",\"nid\":%d,\"dsd\":\"\",\"onid\":%d,\"tsid\":%d,\"sid\":%d,\"name\":\"%s\",\"longName\":\"%s\",\"description\":\"OIPF (SD&amp;S) - TCServiceData doesn’t support yet!\",\"authorised\":true,\"genre\":null,\"hidden\":false,\"idType\":%d,\"channelMaxBitRate\":0,\"manualBlock\":false,\"majorChannel\":1,\"ipBroadcastID\":\"rtp://1.2.3.4/\",\"locked\":false}", channelType, 1, channel->Nid(), channel->Tid(), channel->Sid(), currentChannel, currentChannel, 15);
+
+         browserComm->SendToBrowser(cmd);
+         free(cmd);
       }
    }
 }
